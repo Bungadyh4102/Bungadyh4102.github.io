d1393a1c4dbc780f868425f61329736f83370941
Bungadyh4102 <162246288+Bungadyh4102@users.noreply.github.com>
2024-10-02 16:02:35 +0800

    Add files via upload

---
+++ Posttest_Web_4/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const popUpBox = document.getElementById('popUpBox');
+    const closeBtn = document.querySelector('.close');
+
+    popUpBox.style.display = 'block';
+
+    closeBtn.addEventListener('click', function() {
+        popUpBox.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === popUpBox) {
+            popUpBox.style.display = 'none';
+        }
+    });
+
+    const themeButton = document.getElementById('themeButton');
+    const body = document.body;
+
+    themeButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        body.classList.toggle('light-mode');
+        themeButton.textContent = body.classList.contains('dark-mode') ? '🌙' : '🌞';
+    });
+
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
